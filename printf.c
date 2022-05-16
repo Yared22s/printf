ff51f75adc78ec9d53eaf2416648578fa4882c9f
Yared S <s.yared25@gmail.com>
2022-05-16 23:16:16 +0300

    Printf Collaborative Project

--- printf.c
+++ printf.c
@@ -9,34 +9,34 @@ int _printf(const char *format, ...)
 {
 	va_list list;
 	unsigned int i = 0, characters_number = 0;
-
+	
 	if (!format)
 		return (-1);
+
 	va_start(list, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+			else if (format[i + 1] == '%')
 			{
-				if (format[i + 1] == '\0')
-					return (-1);
-
-				else if (format[i + 1] == '%')
-				{
-					_putchar('%');
-					characters_number++;
-					i++;
-				}
-				else if (cmp_func(format[i + 1]) != NULL)
-				{
-					characters_number += (cmp_func(format[i + 1]))(list);
-					i++;
-				}
-				else
-				{
-					_putchar(format[i]);
-					characters_number++;
-				}
+				_putchar('%');
+				characters_number++;
+				i++;
 			}
+			else if (cmp_func(format[i + 1]) != NULL)
+			{
+				characters_number += (cmp_func(format[i + 1]))(list);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				characters_number++;
+			}
+		}
 		else
 		{
 			_putchar(format[i]);
